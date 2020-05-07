5c9246ac762140000d4934ad390cd97208ec3412
HiiroTakeuchi <63570092+HiiroTakeuchi@users.noreply.github.com>
2020-05-07 16:45:22 +0900

    Update python.py

--- python.py
+++ python.py
@@ -10,14 +10,14 @@ def tan(x):
     import math
     return math.tan(math.radians(x))
 
-i = 0
+x = 0
 
 
 while x <= 360:
-    a = sin(i)
-    b = cos(i)
-    c = tan(i)
-    print("sin{}°= {}".format(i,a))
-    print("cos{}°= {}".format(i,b))
-    print("tan{}°= {}".format(i,c))
-    i += 15
+    a = sin(x)
+    b = cos(x)
+    c = tan(x)
+    print("sin{}°= {}".format(x,a))
+    print("cos{}°= {}".format(x,b))
+    print("tan{}°= {}".format(x,c))
+    x += 15
